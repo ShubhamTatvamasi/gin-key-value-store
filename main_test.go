5e67931165c74dfd9a80fc80f49e3f5d2f3abd5f
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2023-09-24 08:34:13 +0530

    modified:   main.go

--- main_test.go
+++ main_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ShubhamTatvamasi/gin-key-value-store/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,7 +15,7 @@ import (
 func TestGetValueEmpty(t *testing.T) {
 
 	gin.SetMode(gin.ReleaseMode)
-	router := setupRouter()
+	router := routes.SetupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/get?key=name", nil)
@@ -30,7 +31,7 @@ func TestGetValueWithValue(t *testing.T) {
 	newKeyValue := "key=name&value=shubham"
 
 	gin.SetMode(gin.ReleaseMode)
-	router := setupRouter()
+	router := routes.SetupRouter()
 
 	// Set a key with value
 	w1 := httptest.NewRecorder()
@@ -53,7 +54,7 @@ func TestPostKeyValue(t *testing.T) {
 	newKeyValue := "key=name&value=shubham"
 
 	gin.SetMode(gin.ReleaseMode)
-	router := setupRouter()
+	router := routes.SetupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/post", strings.NewReader(newKeyValue))
@@ -71,7 +72,7 @@ func TestSubscribeKey(t *testing.T) {
 	newKeyValue := "key=name"
 
 	gin.SetMode(gin.ReleaseMode)
-	router := setupRouter()
+	router := routes.SetupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/subscribe", strings.NewReader(newKeyValue))
@@ -89,7 +90,7 @@ func TestUnsubscribeKey(t *testing.T) {
 	newKeyValue := "key=name"
 
 	gin.SetMode(gin.ReleaseMode)
-	router := setupRouter()
+	router := routes.SetupRouter()
 
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("POST", "/unsubscribe", strings.NewReader(newKeyValue))
---
+++ routes/SetupRouter.go
@@ -0,0 +1,109 @@
+package routes
+
+import (
+	"embed"
+	"encoding/json"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed templates/*
+var templates embed.FS
+
+// Setup Router for Gin Server
+func SetupRouter() *gin.Engine {
+
+	// Store all key value pairs in map
+	store := map[string]string{}
+	// List of all the subscribed keys
+	subscription := make(map[string]struct{})
+
+	// Route the gin web server
+	r := gin.Default()
+
+	// Serve static files
+	r.Static("/assets", "./templates/assets")
+
+	// Home page
+	r.GET("/", func(c *gin.Context) {
+
+		// Read the contents of index.html from the embedded filesystem
+		indexHTML, err := templates.ReadFile("templates/index.html")
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		// Sending empty string, so rendering nothing.
+		c.Data(http.StatusOK, "text/html; charset=utf-8", indexHTML)
+
+	})
+
+	// Get function for getting a specfic value for a key
+	r.GET("/get", func(c *gin.Context) {
+
+		// Return Value
+		c.String(200, store[c.Query("key")])
+
+	})
+
+	// Post function for adding or updating values
+	r.POST("/post", func(c *gin.Context) {
+
+		key := c.PostForm("key")
+		value := c.PostForm("value")
+		store[key] = value
+
+		// Return Key=Value
+		c.String(200, key+"="+value)
+
+	})
+
+	// Subscribe to keys for regular updates
+	r.POST("/subscribe", func(c *gin.Context) {
+
+		key := c.PostForm("key")
+		subscription[key] = struct{}{}
+
+		// Return subscribed key
+		c.String(200, key)
+
+	})
+
+	// Unsubscribe key
+	r.POST("/unsubscribe", func(c *gin.Context) {
+
+		key := c.PostForm("key")
+		delete(subscription, key)
+
+		// Return unsubscribed key
+		c.String(200, key)
+
+	})
+
+	// Keep connection alive for any updated on subscription
+	r.GET("/stream", func(c *gin.Context) {
+
+		// Add header for setting Server Sent Events to live stream
+		w := c.Writer
+		w.Header().Set("Content-Type", "text/event-stream")
+
+		keyValues := make(map[string]interface{})
+
+		// Collect of subscribed key values
+		for key := range subscription {
+			keyValues[key] = store[key]
+		}
+
+		// Package all keys in JSON
+		empData, _ := json.Marshal(keyValues)
+		jsonString := string(empData)
+
+		// Send JSON back to live stream
+		w.Write([]byte("data:" + jsonString + "\n\n"))
+
+	})
+
+	return r
+}
